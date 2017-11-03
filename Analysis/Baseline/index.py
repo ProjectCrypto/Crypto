697de4af94d5dbb76c3107b012e3eb44399b83b6
eentr928 <eentr928@live.kutztown.edu>
2017-11-03 19:12:32 -0400

    Updated Report API again <3

--- Analysis/Baseline/index.py
+++ Analysis/Baseline/index.py
@@ -21,21 +21,27 @@ class Analysis(object):
     
         fig,ax = plt.subplots(figsize=(12,8))
         ax.plot(x,y,c='c')
-        ax.set_title('Exchange Rate in Time')
+        ax.set_title('Exchange Rate in Time',fontsize=24)
         ax.set_xlabel('Date')
         ax.set_ylabel('Price($)')
-        
+        ax.legend(loc='best',fancybox=True, shadow=True)
         
         plt.tight_layout()
         plot1 = os.path.join(parent.output,'plot1.png')
         plt.savefig(plot1, bbox_inches='tight',transparent=True)
         
+        text = """
+        Welcome to Report! An API to help mitigate the tasks in presenting
+        your analysis. Add sections with header or tables and plots. I cant
+        say I have everything figured out but this should help those who
+        are savy with Python but not HTML. <3
+        """    
         
         report = Report(name = 'Baseline', path = parent.output)
-        text = "Welcome to 'Report`! A module to help mitigate those tasks."
+        
         report.add(header = 'Introduction', text = text)
         report.add(plot= plot1)
-        report.add(header = 'Summry', table = data)
+        report.add(header = 'Summary', table = data.head())
         
         report.create()
         
