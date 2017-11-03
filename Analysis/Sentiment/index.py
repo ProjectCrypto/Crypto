b460352ab94e3d9040fab44c897ffcc5647d544f
eentr928 <eentr928@live.kutztown.edu>
2017-11-02 20:43:45 -0400

    Trying to add more websites and reformat

--- Analysis/Sentiment/index.py
+++ Analysis/Sentiment/index.py
@@ -5,6 +5,14 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
+import nltk.classify.util
+#from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize,sent_tokenize
+from nltk.corpus import wordnet
+
+stopwords = nltk.corpus.stopwords.words('english')
+
 class Analysis(object):
     def __init__(self):
         pass
@@ -16,9 +24,19 @@ class Analysis(object):
         data = parent.execute(query)
         
         for index, group in data.iterrows():
-            sentences = group['Content'].split('.')
-            #import pdb;pdb.set_trace()
+            content = group['Content'].encode('ascii',errors='ignore')
+            for line in sent_tokenize(content):
+                tokens = self.stop_words(line)
+                # Need to do something with this
+               # import pdb;pdb.set_trace()
         
+
+        
+    def stop_words(self,sentence):
+        lowerCase = sentence.lower()
+        tokens    = word_tokenize(lowerCase)
+        
+        return [x for x in tokens if x not in stopwords]
         
         
 if __name__ == "__main__":
